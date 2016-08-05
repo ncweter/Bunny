541d7f93bb6b1087979ffa5914eaeb1e0ad2ba21
Max Wu <maxwu@max-wudemacbook-air.local>
2016-08-05 16:01:35 +0800

    4th

--- HoppyBunny/GameScene.swift
+++ HoppyBunny/GameScene.swift
@@ -9,12 +9,19 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    var hero: SKSpriteNode!
     override func didMoveToView(view: SKView) {
         /* Set up your scene here */
+        
+        /* Recursive node search for 'hero' (child of referenced node) */
+        hero = self.childNodeWithName("//hero") as! SKSpriteNode
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
         /* Called when a touch begins */
+        
+        /* Apply vertical impulse */
+        hero.physicsBody?.applyImpulse(CGVectorMake(0, 250))
     }
     
     override func update(currentTime: CFTimeInterval) {
