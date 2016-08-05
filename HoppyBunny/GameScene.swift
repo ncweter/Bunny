efd69cb858bc1374e22dc4b04842c5949afe8e70
Max Wu <maxwu@max-wudemacbook-air.local>
2016-08-05 16:59:36 +0800

    5th

--- HoppyBunny/GameScene.swift
+++ HoppyBunny/GameScene.swift
@@ -10,6 +10,9 @@ import SpriteKit
 
 class GameScene: SKScene {
     var hero: SKSpriteNode!
+    var sinceTouch : CFTimeInterval = 0
+    let fixedDelta: CFTimeInterval = 1.0/60.0 /* 60 FPS */
+    
     override func didMoveToView(view: SKView) {
         /* Set up your scene here */
         
@@ -22,9 +25,41 @@ class GameScene: SKScene {
         
         /* Apply vertical impulse */
         hero.physicsBody?.applyImpulse(CGVectorMake(0, 250))
+        
+        /* Play SFX */
+        let flapSFX = SKAction.playSoundFileNamed("sfx_flap", waitForCompletion: false)
+        self.runAction(flapSFX)
+        
+        /* Apply subtle rotation */
+        hero.physicsBody?.applyAngularImpulse(1)
+        
+        /* Reset touch timer */
+        sinceTouch = 0
     }
     
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+        
+        /* Grab current velocity */
+        let velocityY = hero.physicsBody?.velocity.dy ?? 0
+        
+        /* Check and cap vertical velocity */
+        if velocityY > 400 {
+            hero.physicsBody?.velocity.dy = 400
+        }
+        
+        /* Apply falling rotation */
+        if sinceTouch > 0.1 {
+            let impulse = -20000 * fixedDelta
+            hero.physicsBody?.applyAngularImpulse(CGFloat(impulse))
+        }
+        
+        /* Clamp rotation */
+        hero.zRotation.clamp(CGFloat(-20).degreesToRadians(),CGFloat(30).degreesToRadians())
+        hero.physicsBody?.angularVelocity.clamp(-2, 2)
+        
+        /* Update last touch timer */
+        sinceTouch+=fixedDelta
     }
+
 }
